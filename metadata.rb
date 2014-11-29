a1c8bf7f867ec636b369500ac26acde44e2feca9
Steffen Gebert <steffen.gebert@typo3.org>
2014-11-30 00:20:16 +0100

    met

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email ''
 license          ''
 description      'Installs/Configures site-otrs-sectypo3org'
 long_description 'Installs/Configures site-otrs-sectypo3org'
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'perl', '= 0.10.0'
