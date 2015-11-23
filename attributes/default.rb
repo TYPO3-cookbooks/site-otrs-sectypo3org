741610989a5fd52151c9b4f719da0bc60dbd1d75
Steffen Gebert <steffen.gebert@typo3.org>
2015-11-24 00:01:05 +0100

    Add comments for attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,10 @@
-# override the default vhost template
+#<> override the default vhost template
 default['otrs']['apache2']['vhost_source'] = "site-otrs-sectypo3org"
 
+#<> Setup email address
 default['otrs']['kernel_config']['email'] = "otrs@typo3.org"
+#<> Setup organization name
 default['otrs']['kernel_config']['organization'] = "TYPO3 Association"
 
+#<> Name of the SSL certificate to deploy
 default['site-otrs-sectypo3org']['ssl_certificate'] = "wildcard.typo3.org"
