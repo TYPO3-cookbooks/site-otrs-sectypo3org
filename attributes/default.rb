4fa78d00423ebc57cfe975889adc40860c3761d8
Steffen Gebert <steffen.gebert@typo3.org>
2015-11-23 23:51:07 +0100

    Update kernel_config attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,7 @@
 # override the default vhost template
 default['otrs']['apache2']['vhost_source'] = "site-otrs-sectypo3org"
 
+default['otrs']['kernel_config']['email'] = "otrs@typo3.org"
+default['otrs']['kernel_config']['organization'] = "TYPO3 Association"
+
 default['site-otrs-sectypo3org']['ssl_certificate'] = "wildcard.typo3.org"
