2f2b4cbc416849169b77438490b8842f922a9da7
Steffen Gebert <steffen.gebert@typo3.org>
2017-02-10 14:06:08 +0100

    [TESTS] Test authenticated HTTP

--- test/integration/default/inspec/securityotrs_spec.rb
+++ test/integration/default/inspec/securityotrs_spec.rb
@@ -18,4 +18,15 @@ control 'securityotrs-1' do
     its('stdout') { should include 'WWW-Authenticate: Digest realm="t3secteam"' }
   end
 
+  describe command('curl -i http://otrs.vagrant --digest --user test:test') do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should include 'HTTP/1.1 200' }
+    its('stdout') { should include '<meta http-equiv="refresh" content="0; URL=/otrs/index.pl" />' }
+  end
+
+  describe command('curl -I http://otrs.vagrant/otrs/index.pl --digest --user test:test') do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should include 'HTTP/1.1 200' }
+  end
+
 end
